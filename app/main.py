1bec7c658ae1d8d56d6456d4478b884766bb91bd
TemsyChen <70554318+TemsyChen@users.noreply.github.com>
2021-06-16 10:58:27 -0700

    deleted files

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Label("Hello World!", style={'fontSize':40, 'textAlign':'left'})
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
